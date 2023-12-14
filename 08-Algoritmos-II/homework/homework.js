d4e0fc9b27f9ee7cf882f851963c83e953829f3c
Joel Fernandez <ejoelf10@hotmail.com>
2023-12-14 19:38:37 +0100

    Homework resuelta

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,4 +1,7 @@
-'use strict'
+"use strict";
+
+const { merge } = require("@11ty/eleventy/src/TemplateData");
+
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,8 +9,23 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) return array;
 
+  let pivot = array[0];
+  let izq = [];
+  let der = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      izq.push(array[i]);
+    } else {
+      der.push(array[i]);
+    }
+  }
+  return quickSort(izq).concat(pivot).concat(quickSort(der));
 }
+const array = [3, 7, 9, 1, 6, 4, 8, 2, 5, 10];
+console.log(quickSort(array));
 
 function mergeSort(array) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
@@ -15,8 +33,32 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-  //okey probemos ahora
+  if (array.length <= 1) return array; //Caso base
+
+  let mitadArray = Math.floor(array.length / 2); //Dividimos el array en 2
+  let izq = array.slice(0, mitadArray); //parte izquierda
+  let der = array.slice(mitadArray); //parte derecha
+
+  function merge(arr1, arr2) {
+    let i = 0;
+    let j = 0;
+
+    let ordenado = [];
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] < arr2[i]) {
+        ordenado.push(arr1[i]);
+        i++;
+      } else {
+        ordenado.push(arr2[j]);
+        j++;
+      }
+    }
+    return ordenado.concat(arr1.slice(i)).concat(arr2.slice(j));
+  }
+  return merge(mergeSort(izq), mergeSort(der)); //merge sera la nueva función
 }
+console.log(mergeSort(array));
 
 // No modificar nada debajo de esta línea
 // --------------------------------
